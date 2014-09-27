f6156ae074e5bcdd229dc8e2f3f4a2bb885b353f
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2014-09-28 01:36:22 +0400

    generate fuel

--- resources/public/img/generate.sh
+++ resources/public/img/generate.sh
@@ -6,8 +6,7 @@ path_normal=$template
 path_fire="#f6ff07"
 
 rocket_normal=$path_normal
-
-mkdir generated
+fuel=$path_fire
 
 for i in 0 1 2 3 4
 do
@@ -18,3 +17,8 @@ done
 convert templates/rocket.png  -fill $rocket_normal -opaque $template generated/rocket.png
 convert templates/rocket_fire.png  -fill $rocket_normal -opaque $template generated/rocket_fire.png
 convert templates/shuffle.png  -fill $path_normal -opaque $template generated/shuffle.png
+convert templates/selected.png  -fill $path_normal -opaque $template generated/selected.png
+
+convert templates/empty_rocket.png -fill $path_fire -stroke $path_fire -draw "rectangle 9 23 26 49" generated/fuel_3.png
+convert templates/empty_rocket.png -fill $path_fire -stroke $path_fire -draw "rectangle 9 32 26 49" generated/fuel_2.png
+convert templates/empty_rocket.png -fill $path_fire -stroke $path_fire -draw "rectangle 9 41 26 49" generated/fuel_1.png
