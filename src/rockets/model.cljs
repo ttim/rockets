1e2ab2271ba27b0c16efd4b1e87dc8528d96a322
asiunov <a.siunov@corp.vk.com>
2014-09-27 18:58:27 +0400

    model updated

--- src/rockets/model.cljs
+++ src/rockets/model.cljs
@@ -5,46 +5,47 @@
    :player1 "First Rocketeer"
    :player2 "Second Rocketeer"})
 
-(def size-n 5)
+(def size-n 8)
+(def size-m 8)
+(def rockets-cnt 4)
 
 (defn pos [x y]
-  {
-    :x x
-    :y y
-    })
+  {:x x
+   :y y})
 
 ; 0 - top, 1 - right, 2 - down, 3 - left
 (def cell-types
-  [
-    [[2] [] [0] []]
-    [[1 3] [0 3] [] [0 1]]
-    [[1] [0] [] []]
-    [[1 2 3] [0 2 3] [0 1 3] [0 1 2]]
-    [[] [3] [] [1]]
-    [[] [] [] []]
-    ])
+  [[[2] [] [0] []]
+   [[1 3] [0 3] [] [0 1]]
+   [[1] [0] [] []]
+   [[1 2 3] [0 2 3] [0 1 3] [0 1 2]]
+   [[] [3] [] [1]]
+   [[] [] [] []]])
 
-(defn cell
-  [orientation cell-type locked]
-  {
-    :orientation orientation
-    :cell-type   cell-type
-    :locked      locked
-    })
+(defn cell [cell-type orientation locked]
+  {:cell-type   cell-type
+   :orientation orientation
+   :locked      locked
+   })
 
 (defn generate-rocket [state progress]
-  {
-    :state    state
-    :progress progress
-    })
+  {:state    state
+   :progress progress})
 
-(defn generate-field
-  [] [])
+(defn generate-field []
+  [])
+
+(defn generate-board-cell []
+  (cell (rand-int (clojure.core/count cell-types)) (rand-int 4) false))
+
+(defn generate-cells []
+  (into (vector) (for [i (range 0 size-n)]
+                   (into (vector) (for [j (range 0 size-m)]
+                                    (generate-board-cell))))))
 
 (defn generate-board []
-  {
-    :selected (pos 0 0)
-    :fields   []})
+  {:selected (pos 0 0)
+   :cells    (generate-cells)})
 
 (defn generate-rockets
   [b1 b2] [])
