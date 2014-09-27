9f8a2e2bc3212a988bcb8d0c5a14db67edc7f9c6
Timur Abishev <abishev.timur@gmail.com>
2014-09-27 19:45:24 +0400

    render n*m sprites in table

--- src/rockets/game.cljs
+++ src/rockets/game.cljs
@@ -13,10 +13,10 @@
 
 (q/defcomponent
   FieldComponent [cells selected]
-  (html [:div (for [x (range 0 model/size-n)
-                    y (range 0 model/size-m)
-                    :let [cell (nth (nth cells x) y)]]
-                (CellComponent cell false))]))
+  (html [:table (for [x (range 0 model/size-n)]
+                  [:tr (for [y (range 0 model/size-m)
+                             :let [cell (nth (nth cells x) y)]]
+                         [:td (CellComponent cell false)])])]))
 
 (q/defcomponent
   BoardComponent [board]
