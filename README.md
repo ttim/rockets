03696075ded602cbc9735f44c5c0a1a9e6b00a27
Timur Abishev <tabishev@UNIT-594.local>
2014-09-27 13:29:13 +0400

    initial figwheel commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 Awesome Rocketeers Go Ahead!
-
---
+++ src/rockets/core.cljs
@@ -0,0 +1,15 @@
+(ns rockets.core
+  (:require
+    [figwheel.client :as fw]))
+
+(enable-console-print!)
+
+;; define your app data so that it doesn't get over-written on reload
+;; (defonce app-data (atom {}))
+
+(println "Edits to this text should show up in your developer console.")
+
+(fw/watch-and-reload
+ :jsload-callback (fn []
+                    ;; (stop-and-start-my app)
+                    ))
