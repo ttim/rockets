b2eb912a6b31eaa10fd09b5c383275877bf30cff
Julia Beliaeva <julia.beliaeva@jetbrains.com>
2014-10-15 05:41:54 +0400

    add version info into commit message of gh-pages

--- deploy_ghpages.sh
+++ deploy_ghpages.sh
@@ -2,11 +2,12 @@
 rm -rf tmp_git
 git clone https://github.com/ttim/rockets.git tmp_git
 cd tmp_git
+version=`git log -1 --format='oneline'`
 git checkout gh-pages
 rm -rf ./*
 cp -rf ../www/* ./
 git add --all
-git commit -m "new version"
+git commit -m "upload $version"
 git push origin gh-pages
 cd ..
 rm -rf tmp_git
