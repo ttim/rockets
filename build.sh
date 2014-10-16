1ed397f90b996ee0c7b825b114732ded97d35d87
Julia Beliaeva <julia.beliaeva@jetbrains.com>
2014-10-16 05:59:27 +0400

    copy about to build

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ cp -rf resources/public/sound www/
 rm www/img/generate.sh
 cp resources/public/index.html www/
 cp resources/public/CNAME www/
+cp resources/public/about.html www/
 cat www/index.html | sed 's/js\/compiled\/rockets\.js/rockets.min.js/g' | sed 's/<script src="js\/compiled\/out\/goog\/base.js" type="text\/javascript"><\/script>//g' | sed 's/<script type="text\/javascript">goog.require("rockets.core");<\/script>//g' > www/index_new.html
 mv -f www/index_new.html www/index.html
 
